7355e6a91032fcb7588f0c03f62a00c2cbb4fa6e
Snavyy <1337Snavy@gmail.com>
2022-09-28 10:00:08 +0200

    added files for ex05

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Exit status code
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
