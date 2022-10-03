aa8f8d43ea0784a411564d9b71cd04357ac04971
Snavyy <1337Snavy@gmail.com>
2022-10-03 15:08:50 +0200

    added files for ex09

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * func - function
+ * @x: value
+ */
+void	func(int x)
+{
+	if (x > 9)
+		_putchar('0' + x / 10);
+	else
+		_putchar(' ');
+	_putchar('0' + x % 10);
+}
+
+/**
+ * jack_bauer - function
+ */
+void	times_table(void)
+{
+	int	x;
+	int	y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			func(x * y);
+			if (y == 9)
+				continue;
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
