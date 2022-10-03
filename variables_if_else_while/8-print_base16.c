a816c2fc78b38806b0eb66ec784e7c6ffe93068d
Snavyy <1337Snavy@gmail.com>
2022-10-03 09:50:30 +0200

    added files for ex08

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Exit status code
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
