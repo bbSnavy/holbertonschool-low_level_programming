367b3bcefae18949e65e0c0a20e390a7f57e4bc9
Snavyy <1337Snavy@gmail.com>
2022-10-03 09:35:19 +0200

    added files for ex02

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Exit status code
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
