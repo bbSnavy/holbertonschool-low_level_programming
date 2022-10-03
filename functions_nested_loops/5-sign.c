6040e7e72fc7a9ea8dbe6dc714fa1ba9ffc928ea
Snavyy <1337Snavy@gmail.com>
2022-10-03 14:23:58 +0200

    added files for ex05

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_sign - function
+ * @n: value
+ *
+ * Return: sign of n
+ */
+int print_sign(int n)
+{
+	if (n == 0)
+		return ('0')
+	if (n < 0)
+		return ('-');
+	else
+		return ('+');
+	return (0);
+}
