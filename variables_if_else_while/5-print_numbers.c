37c143d929a2e06ab0c95c6d7a07cc3a1130ad31
Snavyy <1337Snavy@gmail.com>
2022-10-03 09:45:48 +0200

    added files for ex05

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main function
+ *
+ * Return: Exit status code
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
