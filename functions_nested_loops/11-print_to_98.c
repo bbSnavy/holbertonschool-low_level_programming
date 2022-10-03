5ed3e18c64f1e147e9262a77a67e3a26adf2c6f5
Snavyy <1337Snavy@gmail.com>
2022-10-03 15:35:13 +0200

    added files for ex11

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function
+ * @n: number
+ */
+void print_to_98(int n)
+{
+	for (; n < 99; n++)
+	{
+		printf("%d", n);
+		if (n == 98)
+			continue;
+		printf(", ");
+	}
+}
