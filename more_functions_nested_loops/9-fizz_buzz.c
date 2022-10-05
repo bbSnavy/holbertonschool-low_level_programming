be1cb5e9b6ff81389d80f4206c70d453ed81d02b
Snavyy <1337Snavy@gmail.com>
2022-10-05 09:45:09 +0200

    fixed files for ex09

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,13 +9,12 @@
  */
 int	fizzbuzz(int n)
 {
-	if (n == 0)
+	if (n > 100)
 		return (0);
-	return (fizzbuzz(n - 1) + printf(&"\0%d"[!printf("%s",
-		&"\0Fizz\0Buzz\0\0\0FizzBuzz"[(int)((float)((n % 3
-		== 0) + 2 * (n % 5 == 0)) * (float)((n % 3 == 0) +
-		2 * (n % 5 == 0)) * 1.5f)])], n) * printf("%s",
-		&" \0\n"[(n == 100) * 2]) * 0);
+	return (printf(&"\0%d"[!printf("%s", &"\0Fizz\0Buzz\0\0\0FizzBuzz"[(int)
+		((float)((n % 3 == 0) + 2 * (n % 5 == 0)) * (float)((n % 3 == 0) + 2
+		* (n % 5 == 0)) * 1.5f)])], n) * printf("%s", &" \0\n"[(n == 100) *
+		2]) * 0 + fizzbuzz(n + 1));
 }
 
 /**
@@ -25,5 +24,5 @@ int	fizzbuzz(int n)
  */
 int	main(void)
 {
-	return (fizzbuzz(100));
+	return (fizzbuzz(1));
 }
