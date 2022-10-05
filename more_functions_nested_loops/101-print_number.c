b0c475b5ca49da0e65936c60cef664b6ff968578
Snavyy <1337Snavy@gmail.com>
2022-10-05 10:30:07 +0200

    added files for ex12

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - function
+ * @n: int
+ */
+void	print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
+	else if (n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		print_number(n / 10);
+		_putchar('0' + n % 10);
+	}
+}
