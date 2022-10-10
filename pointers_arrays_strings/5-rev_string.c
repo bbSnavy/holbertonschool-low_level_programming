ed8429b85aa9810b1a192bde92177a1a63af9355
Snavyy <1337Snavy@gmail.com>
2022-10-10 12:17:59 +0200

    added files for ex05

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - function
+ * @str: char ptr
+ */
+void	rev_string(char *str)
+{
+	int	l;
+	int	i;
+	char	c;
+
+	for (l = 0; str[l]; l++)
+		;
+	for (i = 0; i < l; i++)
+	{
+		c = str[i];
+		str[i] = str[l - 1 - i];
+		str[l - 1 - i] = c;
+	}
+}
