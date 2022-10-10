76a94997972de7563de8aa9962221eab79b7f0a8
Snavyy <1337Snavy@gmail.com>
2022-10-10 12:23:27 +0200

    added files for ex07

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * puts_half - function
+ * @str: char ptr
+ */
+void	puts_half(char *str)
+{
+	int	i;
+
+	for (i = 0; str[i]; i++)
+		;
+	for (i /= 2; str[i]; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
