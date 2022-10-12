137827a5f8d5ccf8fe026725d6b42e9314d4bb22
Snavyy <1337Snavy@gmail.com>
2022-10-12 09:17:13 +0200

    fixed files for ex09

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - function
- * @n: int
+ * print_number_logic - function
+ * @n: unsigned int
  */
-void	print_number(int n)
+void	print_number_logic(unsigned int n)
 {
-	int	x;
-
-	if (n < -1000)
-	{
-		print_number(n / 1000);
-		x = (-n) % 1000;
-		if (x < 1000)
-			_putchar('0');
-		print_number(0 - (n % 1000));
-	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		print_number(-n);
-	}
-	else if (n < 10)
+	if (n < 10)
 	{
 		_putchar('0' + n);
 	}
 	else
 	{
-		print_number(n / 10);
-		print_number(n % 10);
+		print_number_logic(n / 10);
+		print_number_logic(n % 10);
 	}
 }
 
+/**
+ * print_number - function
+ * @n: int
+ */
+void	print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		print_number_logic((unsigned int) -n);
+	}
+	else
+	{
+		print_number_logic((unsigned int) n);
+	}
+}
