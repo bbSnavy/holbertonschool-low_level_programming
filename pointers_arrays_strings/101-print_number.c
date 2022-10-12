99d2efca0266591db028dd1e794b8d0bd4b10772
Snavyy <1337Snavy@gmail.com>
2022-10-12 09:08:51 +0200

    fixed files for ex09

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -6,9 +6,14 @@
  */
 void	print_number(int n)
 {
+	int	x;
+
 	if (n < -1000)
 	{
 		print_number(n / 1000);
+		x = (-n) % 1000;
+		if (x < 100)
+			_putchar('0');
 		print_number(0 - (n % 1000));
 	}
 	else if (n < 0)
