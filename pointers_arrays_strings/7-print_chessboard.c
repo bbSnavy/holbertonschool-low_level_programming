0ae8d43e8f5764a00c79a87baa91969befdb72df
Snavyy <1337Snavy@gmail.com>
2022-10-13 21:59:23 +0200

    added files for ex06

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard
+ * @a: char ptr ptr
+ */
+void	print_chessboard(char (*a)[8])
+{
+	int	x;
+	int	y;
+
+	for (y = 0; y < 8; y++)
+	{
+		for (x = 0; x < 8; x++)
+		{
+			_putchar(a[y][x]);
+		}
+		_putchar('\n');
+	}
+}
