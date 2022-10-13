3976652123dcb1cc3ee08a78aecef7426e532933
Snavyy <1337Snavy@gmail.com>
2022-10-13 21:56:51 +0200

    fixed files for ex04

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,9 +10,9 @@ char	*_strpbrk(char *s, char *accept)
 	int	x;
 	int	y;
 
-	for (x = 0; accept[x]; x++)
+	for (y = 0; s[y]; y++)
 	{
-		for (y = 0; s[y]; y++)
+		for (x = 0; accept[x]; x++)
 		{
 			if (s[y] == accept[x])
 			{
