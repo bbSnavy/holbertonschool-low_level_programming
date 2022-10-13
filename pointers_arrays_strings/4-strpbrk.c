3e37cfc9ea1ebeaaf94647ec4b7ec24deac27b3f
Snavyy <1337Snavy@gmail.com>
2022-10-13 21:55:09 +0200

    added files for ex04

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -1,20 +1,5 @@
 /**
- * ft_strlen - function
- * @str: char ptr
- *
- * return: int
- */
-int	ft_strlen(char *str)
-{
-	int	x;
-
-	for (x = 0; str[x]; x++)
-		;
-	return (x);
-}
-
-/**
- * _strpbrk - function
+ * strpbrk - function
  * @s: char ptr
  * @accept: char ptr
  *
@@ -22,23 +7,17 @@ int	ft_strlen(char *str)
  */
 char	*_strpbrk(char *s, char *accept)
 {
-	int	l;
 	int	x;
-	int	m;
+	int	y;
 
-	if (s == 0)
-		return (0);
-	l = ft_strlen(accept);
-	if (l < 1)
-		return (s);
-	for (x = 0; s[x]; x++)
+	for (x = 0; accept[x]; x++)
 	{
-		m = 0;
-		while (s[x + m] && s[x + m] == accept[m])
+		for (y = 0; s[y]; y++)
 		{
-			m++;
-			if (m == l)
-				return (&s[x]);
+			if (s[y] == accept[x])
+			{
+				return (&s[y]);
+			}
 		}
 	}
 	return (0);
