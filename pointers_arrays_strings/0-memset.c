d22047afb07f6bc55a27824f6ac5c2caaf1cc42f
Snavyy <1337Snavy@gmail.com>
2022-10-13 19:32:01 +0200

    added files for ex00

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+/**
+ * _member - function
+ * @s: char ptr
+ * @b: char
+ * @n: unsigned int
+ *
+ * Return: char ptr
+ */
+char	*_memset(char *s, char b, unsigned int n)
+{
+	int	x;
+
+	if (s == 0)
+		return (0);
+	x = 0;
+	while (x < n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s)
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -20,3 +20,5 @@ char	*cap_string(char *);
 char	*leet(char *);
 char	*rot13(char *);
 void	print_number(int n);
+char	*infinite_add(char *n1, char *n2, char *r, int size_r);
+char	*_memset(char *s, char b, unsigned int n);
