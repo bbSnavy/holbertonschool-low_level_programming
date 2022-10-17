c8d96a1a65a822b9d5fc3c9df0b7fad7e62f967c
Snavyy <1337Snavy@gmail.com>
2022-10-17 16:09:27 +0200

    added files for ex01

---
+++ argc_argv/1-args.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * ft_putnbr - function
+ * @n: long
+ */
+void	ft_putnbr(long n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		ft_putnbr(-n);
+	}
+	else if (n < 10)
+	{
+		_putchar('0' + n);
+	}
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+}
+
+/**
+ * main - function
+ * @argc: int
+ * @argv: char ptr ptr
+ *
+ * Return: int
+ */
+int	main(int argc, char **argv)
+{
+	(void) argv;
+	ft_putnbr((long) argc);
+	_putchar('\n');
+	return (0);
+}
