1b782c725fc3fdde1c24704e59d1c275fd42077c
snavy <1337snavy@gmail.com>
2022-10-24 15:50:48 +0000

    added files for ex03

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - function
+ * @width: int
+ * @height: int
+ *
+ * Return: int ptr ptr
+ */
+int	**alloc_grid(int width, int height)
+{
+	int	**r;
+	int	*a;
+	int	x;
+	int	y;
+
+	if (width < 1 || height < 1)
+		return (0);
+	r = (int **) malloc(sizeof(int *) * height);
+	if (r == 0)
+		return (0);
+	for (y = 0; y < height; y++)
+	{
+		a = (int *) malloc(sizeof(int) * width);
+		if (a == 0)
+			return (0);
+		for (x = 0; x < width; x++)
+			a[x] = 0;
+		r[y] = a;
+	}
+	return (r);
+}
