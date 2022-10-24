3f470bf887f8f7058684f3eee38f1f39eb7b8718
snavy <1337snavy@gmail.com>
2022-10-24 17:35:33 +0000

    added files for ex05

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - function
+ * @ac: int
+ * @av: chat ptr ptr
+ *
+ * Return: char ptr
+ */
+char	*argstostr(int ac, char **av)
+{
+	char	r;
+	int	l;
+	int	x;
+	int	y;
+
+	l = 0;
+	for (x = 0; x < ac; x++)
+		for (y = 0; av[x][y]; y++)
+			l++;
+	r = (char *) malloc(sizeof(char) * (l + 1));
+	if (r == 0)
+		return (0);
+	l = 0;
+	for (x = 0; x < ac; x++)
+	{
+		for (y = 0; av[x][y]; y++)
+		{
+			r[l] = av[x][y];
+			l++;
+		}
+	}
+	r[l] = '\0';
+	return (r);
+}
