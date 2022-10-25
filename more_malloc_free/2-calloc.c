05b688ed441dd0299e7f8b0f7ff0dc5e7bb7c8d1
snavy <1337snavy@gmail.com>
2022-10-25 17:06:36 +0000

    fixed files for ex02

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,7 +11,7 @@ void	*_calloc(unsigned int nmemb, unsigned int size)
 	r = malloc(nmemb * size);
 	if (r == 0)
 		return (0);
-	for (x = 0; x < size; x++)
+	for (x = 0; x < (nmemb * size); x++)
 		((unsigned char *) r)[x] = (unsigned char) 0;
 	return (r);
 }
