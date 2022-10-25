95b5fe63aab7d3a0e2c08e6fe608e81b06fa4f8c
snavy <1337snavy@gmail.com>
2022-10-25 16:42:53 +0000

    fixed files for ex01

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -27,11 +27,15 @@ char	*string_nconcat(char *s1, char *s2, unsigned int n)
 		return (0);
 	r[l] = '\0';
 	l = 0;
-	for (x = 0; s1[x]; x++)
-		r[l++] = s1[x];
-	for (x = 0; s2[x] && x < n; x++)
-		r[l++] = s2[x];
-	for (; x < n; x++)
-		r[l++] = '\0';
+	if (s1 != 0)
+		for (x = 0; s1[x]; x++)
+			r[l++] = s1[x];
+	if (s2 != 0)
+	{
+		for (x = 0; s2[x] && x < n; x++)
+			r[l++] = s2[x];
+		for (; x < n; x++)
+			r[l++] = '\0';
+	}
 	return (r);
 }
