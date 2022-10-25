a27d10a0ce90295be2896916a1b3917964558299
snavy <1337snavy@gmail.com>
2022-10-25 17:01:38 +0000

    added files for ex02

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+void	*_calloc(unsigned int nmemb, unsigned int size)
+{
+	void		*r;
+	unsigned int	x;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+	r = malloc(nmemb * size);
+	if (r == 0)
+		return (0);
+	for (x = 0; x < size; x++)
+		r[x] = 0;
+	return (r);
+}
