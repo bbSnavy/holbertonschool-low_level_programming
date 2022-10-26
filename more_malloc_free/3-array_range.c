7ecaf0d3b25508bdfcb400a9768c6f66abd9ab10
snavy <1337snavy@gmail.com>
2022-10-26 13:56:41 +0000

    added files for ex03

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - function
+ * @min: int
+ * @max: int
+ *
+ * Return: int
+int	*array_range(int min, int max)
+{
+	int	*r;
+	int	x;
+
+	if (min > max)
+		return (0);
+	r = (int *) malloc(sizeof(int) * (min - max + 1));
+	if (r == 0)
+		return (0);
+	for (x = min; x <= max; x++)
+		r[x - min] = min;
+	return (r);
+}
