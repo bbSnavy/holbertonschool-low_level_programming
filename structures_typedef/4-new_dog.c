e2e4a30c52441499f96863c6d5f5beafdad62e88
Snavyy <1337Snavy@gmail.com>
2022-10-31 13:22:53 +0100

    fixed files for ex04

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -50,11 +50,6 @@ dog_t	*new_dog(char *name, float age, char *owner)
 	vo = ft_strdup(owner);
 	if (vo == 0)
 		return (0);
-	*r = (dog_t)
-	{
-		.name = vn,
-		.age = age,
-		.owner = vo,
-	};
+	*r = (dog_t) {vn, age, vo};
 	return (r);
 }
