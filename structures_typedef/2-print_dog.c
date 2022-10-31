4a3a3394f3b5bcc7e27530b9bc34c55f4cd12220
Snavyy <1337Snavy@gmail.com>
2022-10-31 12:37:48 +0100

    added files for ex02

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+void	print_dog(struct dog *d)
+{
+	if (d == 0)
+		return;
+	printf("Name: ");
+	if (d->name)
+		printf("%s\n", d->name);
+	else
+		printf("(nil)\n");
+	printf("Age: %f\n", d->age);
+	printf("Owner: ");
+	if (d->owner)
+		printf("%s\n", d->owner);
+	else
+		printf("(nil)\n");
+}
