64b9f3dbea1e94f11f3687329c256076c2e61852
Snavyy <1337Snavy@gmail.com>
2022-10-31 15:06:16 +0100

    added files for ex01

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function
+ * @array: int ptr
+ * @size: size_t
+ * @action: void function ptr
+ */
+void	array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int	x;
+
+	if (array == 0)
+		return;
+	if (size == 0)
+		return;
+	for (x = 0; x < size; x++)
+		action(array[x]);
+}
