6752c1e4b6bb818f67cc63927c900d855e58ab90
snavy <1337snavy@gmail.com>
2022-11-01 18:25:09 +0000

    fixed files for ex01

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -9,15 +9,14 @@ void	print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list		l;
 	unsigned int	x;
-	int		v;
 
-	if (n < 1)
-		return;
 	va_start(l, n);
 	for (x = 0; x < n; x++)
 	{
-		v = va_arg(l, int);
-		printf(&"%d%s\0%d\n"[5 * (x == (n - 1))], v, separator, v);
+		printf("%d", va_arg(l, int));
+		if (separator)
+			printf("%s", separator);
 	}
 	va_end(l);
+	printf("\n");
 }
