5e65c7ffe7c253eb5fc41963d83ef46abf224a9b
snavy <1337snavy@gmail.com>
2022-11-01 18:31:07 +0000

    added files for ex02

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+
+void	print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list		l;
+	unsigned int	x;
+	char		*s;
+
+	va_start(l, n);
+	for (x = 0; x < n; x++)
+	{
+		s = va_arg(l, char *);
+		if (s == 0)
+			printf("(nil)");
+		else
+			printf("%s", s);
+		if ((x < (n - 1)) && separator)
+			printf("%s", separator);
+	}
+	va_end(l);
+	printf("\n");
+}
