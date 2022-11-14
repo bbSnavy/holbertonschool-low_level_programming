f5a441abbf6dd7d1ef96f2b8e2e7752a98a65451
Snavyy <1337Snavy@gmail.com>
2022-11-14 19:14:13 +0100

    fixed files for ex05

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-void	func()	__attribute__	((constructor));
-void	func()
+void	func(void)	__attribute__	((constructor));
+
+/**
+ * func - function
+ */
+void	func(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
