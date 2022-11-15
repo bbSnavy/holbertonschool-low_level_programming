6b5d0f02d4563f8c2137596ba98fd62a6577f6bf
snavy <1337snavy@gmail.com>
2022-11-15 03:59:03 +0000

    added files for ex09

---
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,61 @@
+#include "lists.h"
+
+/**
+ * new_node_index - function
+ * @n: const int
+ *
+ * Return: list_t ptr
+ */
+listint_t	*new_node_index(const int n)
+{
+	listint_t	*r;
+
+	r = (listint_t *) malloc(sizeof(listint_t));
+	if (!r)
+		return (0);
+	r->n = n;
+	r->next = 0;
+	return (r);
+}
+
+/**
+ * insert_nodeint_at_index - function
+ * @head: listint_t ptr ptr
+ * @idx: unsigned int
+ * @n: int
+ *
+ * Return: listint_t ptr
+*/
+listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t	*r;
+	listint_t	*a;
+	listint_t	*b;
+	unsigned int	x;
+
+	if (!head)
+		return (0);
+	r = new_node_index(n);
+	a = *head;
+	if ((*head == 0) || (idx == 0))
+		*head = r;
+	else
+	{
+		for (x = 0; a && (x < idx); x++)
+		{
+			b = a;
+			a = a->next;
+		}
+		if (x == idx)
+		{
+			b->next = r;
+			r->next = a;
+		}
+		else
+		{
+			free(r);
+			return (0);
+		}
+	}
+	return (r);
+}
