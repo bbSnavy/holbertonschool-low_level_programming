e2fe3680e69a7cfe2c9012e646992bdbff306463
snavy <1337snavy@gmail.com>
2022-11-18 16:23:36 +0000

    fixed files for ex12

--- more_singly_linked_lists/101-print_listint_safe.c
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -1,17 +1,6 @@
 #include "lists.h"
 
 /**
- * free_node - function
- * @n: listptr_t ptr
- */
-void	free_node(listptr_t *n)
-{
-	if (!n)
-		return;
-	free(n);
-}
-
-/**
  * listptr_free - function
  * @h: listptr_t ptr ptr
  */
@@ -27,10 +16,10 @@ void	listptr_free(listptr_t **h)
 	{
 		a = b;
 		b = b->n;
-		free_node(a);
+		free(a);
 	}
 	if (b)
-		free_node(b);
+		free(b);
 	*h = 0;
 }
 
@@ -39,34 +28,55 @@ void	listptr_free(listptr_t **h)
  * @h: listptr_t ptr ptr
  * @v: void ptr
  *
- * Return: int
+ * Return: listptr_t ptr
 */
-int	listptr_add(listptr_t **h, void *v)
+listptr_t	*listptr_add(listptr_t **h, void *v)
 {
 	listptr_t	*n;
 	listptr_t	*p;
 
-	if (!h)
+	if (h == 0)
 		return (0);
 	n = (listptr_t *) malloc(sizeof(listptr_t));
-	if (!n)
+	if (n == 0)
 		return (0);
 	n->n = 0;
 	n->v = v;
-	if ((*h) == 0)
+	p = *h;
+	if (p == 0)
 	{
 		*h = n;
-		return (1);
+		return (n);
 	}
-	p = *h;
 	while (p->n)
+		p = p->n;
+	p->n = n;
+	return (n);
+}
+
+/**
+ * listptr_get - function
+ * @h: listptr_t ptr ptr
+ * @v: void ptr
+ *
+ * Return: int
+*/
+int	listptr_get(listptr_t **h, void *v)
+{
+	listptr_t	*p;
+
+	if (h == 0)
+		return (0);
+	p = *h;
+	if (p == 0)
+		return (0);
+	while (p)
 	{
 		if (p->v == v)
-			return (0);
+			return (1);
 		p = p->n;
 	}
-	p->n = n;
-	return (1);
+	return (0);
 }
 
 /**
@@ -82,11 +92,19 @@ size_t	print_listint_safe(const listint_t *head)
 	listint_t	*a;
 
 	if (!head)
+	{
+		printf("[0] [0]\n");
 		exit(98);
+	}
 	p = 0;
 	a = (listint_t *) (void *) head;
 	for (r = 0; a != 0; r++)
 	{
+		if (listptr_get(&p, a))
+		{
+			listptr_free(&p);
+			exit(98);
+		}
 		printf("[%p] %d\n", (void *) a, a->n);
 		if (listptr_add(&p, a) == 0)
 			break;
@@ -94,4 +112,43 @@ size_t	print_listint_safe(const listint_t *head)
 	}
 	listptr_free(&p);
 	return (r);
+}
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+    listint_t *head2;
+    listint_t *node;
+
+    head2 = NULL;
+    add_nodeint(&head2, 0);
+    add_nodeint(&head2, 1);
+    add_nodeint(&head2, 2);
+    add_nodeint(&head2, 3);
+    add_nodeint(&head2, 4);
+    add_nodeint(&head2, 98);
+    add_nodeint(&head2, 402);
+    add_nodeint(&head2, 1024);
+    print_listint_safe(head2);
+    head = NULL;
+    node = add_nodeint(&head, 0);
+    add_nodeint(&head, 1);
+    add_nodeint(&head, 2);
+    add_nodeint(&head, 3);
+    add_nodeint(&head, 4);
+    node->next = add_nodeint(&head, 98);
+    add_nodeint(&head, 402);
+    add_nodeint(&head, 1024);
+    print_listint_safe(head);
+    return (0);
 }
