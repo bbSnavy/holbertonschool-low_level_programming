f3f07614b962a30323d769b8ae9d3286abd10960
snavy <1337snavy@gmail.com>
2022-11-18 16:12:59 +0000

    fixed files for ex12

---
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,97 @@
+#include "lists.h"
+
+/**
+ * free_node - function
+ * @n: listptr_t ptr
+ */
+void	free_node(listptr_t *n)
+{
+	if (!n)
+		return;
+	free(n);
+}
+
+/**
+ * listptr_free - function
+ * @h: listptr_t ptr ptr
+ */
+void	listptr_free(listptr_t **h)
+{
+	listptr_t	*a;
+	listptr_t	*b;
+
+	if (!h)
+		return;
+	b = *h;
+	while (b->n)
+	{
+		a = b;
+		b = b->n;
+		free_node(a);
+	}
+	if (b)
+		free_node(b);
+	*h = 0;
+}
+
+/**
+ * listptr_add - function
+ * @h: listptr_t ptr ptr
+ * @v: void ptr
+ *
+ * Return: int
+*/
+int	listptr_add(listptr_t **h, void *v)
+{
+	listptr_t	*n;
+	listptr_t	*p;
+
+	if (!h)
+		return (0);
+	n = (listptr_t *) malloc(sizeof(listptr_t));
+	if (!n)
+		return (0);
+	n->n = 0;
+	n->v = v;
+	if ((*h) == 0)
+	{
+		*h = n;
+		return (1);
+	}
+	p = *h;
+	while (p->n)
+	{
+		if (p->v == v)
+			return (0);
+		p = p->n;
+	}
+	p->n = n;
+	return (1);
+}
+
+/**
+ * print_listint_safe - function
+ * @head: const listint_t ptr
+ *
+ * Return: size_t
+*/
+size_t	print_listint_safe(const listint_t *head)
+{
+	size_t		r;
+	listptr_t	*p;
+	listint_t	*a;
+
+	if (!head)
+		exit(98);
+	p = 0;
+	a = (listint_t *) (void *) head;
+	for (r = 0; a != 0; r++)
+	{
+		printf("[%p] %d\n", a, a->n);
+		if (listptr_add(&p, a) == 0)
+			break;
+		a = a->next;
+	}
+	listptr_free(&p);
+	return (r);
+}
--- more_singly_linked_lists/lists.h
+++ more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 # define LISTS_H
 
 # include <stdlib.h>
+# include <stddef.h>
 # include <string.h>
 # include <stdio.h>
 
@@ -19,6 +20,19 @@ typedef struct listint_s
 	struct listint_s	*next;
 }	listint_t;
 
+/**
+ * struct listptr_s - singly linked list
+ * @v: val
+ * @n: ptr
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listptr_s
+{
+	void			*v;
+	struct listptr_s	*n;
+}	listptr_t;
+
 size_t		print_listint(const listint_t *h);
 size_t		listint_len(const listint_t *h);
 listint_t	*add_nodeint(listint_t **head, const int n);
@@ -31,5 +45,6 @@ int		sum_listint(listint_t *head);
 listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int		delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t	*reverse_listint(listint_t **head);
+size_t		print_listint_safe(const listint_t *head);
 
 #endif
