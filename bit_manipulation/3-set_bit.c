00bceaad8d99b556727db7e6af1977cdf5563810
snavy <1337snavy@gmail.com>
2022-11-18 06:24:38 +0000

    added files for ex03

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <limits.h>
+
+/**
+* set_bit - function
+* @n: unsigned long int ptr
+* @index: unsigned int
+*
+* Return: int
+*/
+int	set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(ULONG_MAX) * 8))
+		return (-1);
+	*n &= (1 << index);
+	return (1);
+}
+
