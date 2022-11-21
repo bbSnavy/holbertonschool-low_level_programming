6e7b38b2670e7689f7cde0106d16ec1c184f22e2
snavy <1337snavy@gmail.com>
2022-11-21 19:02:14 +0000

    added files for ex02

---
+++ file_io/2-append_test_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+* append_text_to_file - function
+* @filename: const char ptr
+* @text_content: char ptr
+*
+* Return: int
+*/
+int	append_text_to_file(const char *filename, char *text_content)
+{
+	int	f;
+	int	l;
+	int	x;
+
+	if (filename == 0)
+		return (-1);
+	f = open(filename, O_APPEND);
+	if (f < 0)
+		return (-1);
+	l = 1;
+	for (x = 0; text_contenxt && text_content[x]; x++)
+	{
+		l = write(f, &text_content[x], 1);
+		if (l != 1)
+			break;
+	}
+	close(f);
+	if (l != 1)
+		return (-1);
+	return (1);
+}
+
