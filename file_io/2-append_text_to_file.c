ff52c3d872e01ffc2846d6384413776e42117a77
snavy <1337snavy@gmail.com>
2022-11-21 19:16:08 +0000

    fixed files for ex02

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int	append_text_to_file(const char *filename, char *text_content)
 	if (f < 0)
 		return (-1);
 	l = 1;
-	for (x = 0; text_contenxt && text_content[x]; x++)
+	for (x = 0; text_content && text_content[x]; x++)
 	{
 		l = write(f, &text_content[x], 1);
 		if (l != 1)
