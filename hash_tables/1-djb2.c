2f7aceb0de95d49557a9291ee6f02dd204657f90
snavy <1337snavy@gmail.com>
2022-11-24 18:32:17 +0000

    added files for ex01

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - function
+ * @str: const unsigned char ptr
+ *
+ * Return: unsigned long int
+*/
+unsigned long int	hash_djb2(const unsigned char *str)
+{
+	unsigned long int	r;
+	size_t			x;
+
+	r = 5381;
+	for (x = 0; str[x]; x++)
+		r = ((r << 5) + r) + str[x];
+	return (r);
+}
