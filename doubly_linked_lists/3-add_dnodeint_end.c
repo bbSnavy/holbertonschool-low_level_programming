7f19db58874c499cbe157a1234a2c9048653cd1d
snavy <1337snavy@gmail.com>
2022-11-24 05:09:30 +0000

    added files for ex03

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,46 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end_new - function
+ * @n: const int
+ *
+ * Return: dlistint_t ptr
+*/
+dlistint_t	*add_dnodeint_end_new(const int n)
+{
+	dlistint_t	*r;
+
+	r = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (r == 0)
+		return (0);
+	r->n = n;
+	return (r);
+}
+
+/**
+ * add_dnodeint_end - function
+ * @head: dlistint_t ptr ptr
+ * @n: const int
+ *
+ * Return: dlistint_t ptr
+*/
+dlistint_t	*add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t	*r;
+	dlistint_t	*p;
+
+	if (head == 0)
+		return (0);
+	r = add_dnodeint_end_new(n);
+	p = *head;
+	if (p == 0)
+		*head = r;
+	else
+	{
+		while (p->next)
+			p = p->next;
+		p->next = r;
+		r->prev = p;
+	}
+	return (r);
+}
