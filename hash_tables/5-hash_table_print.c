4aece2059ea9f6f4ef1ac7172a2aa2c94dc4f205
snavy <1337snavy@gmail.com>
2022-11-24 19:09:44 +0000

    added files for ex05

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_printf - function
+ * @ht: const hash_table_t ptr
+*/
+void	hash_table_print(const hash_table_t *ht)
+{
+	unsigned long	x;
+	hash_node_t	*p;
+
+	if (ht == 0)
+		return;
+	if (ht->array == 0 || ht->size < 1)
+		return;
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		for (p = ht->array[x]; p; p = p->next)
+			printf("'%s': '%s'", p->key, p->value);
+		if (x < (ht->size - 1))
+			printf(",");
+	}
+	printf("}");
+	printf("\n");
+}
