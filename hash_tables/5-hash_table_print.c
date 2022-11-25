16ae4f8ace24711ed698047d151bda4298a3dd20
snavy <1337snavy@gmail.com>
2022-11-25 06:05:18 +0000

    fixed files for ex05

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,7 +7,9 @@
 void	hash_table_print(const hash_table_t *ht)
 {
 	unsigned long	x;
+	unsigned long	y;
 	hash_node_t	*p;
+	int		f;
 
 	if (ht == 0)
 		return;
@@ -16,14 +18,30 @@ void	hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (x = 0; x < ht->size; x++)
 	{
-		for (p = ht->array[x]; p; p = p->next)
+		if (ht->array[x] == 0)
+			continue;
+		for (p = ht->array[x]; p->next; p = p->next)
 		{
 			printf("'%s': '%s'", p->key, p->value);
-			if (p->next && x < (ht->size - 1))
+			printf(", ");
+		}
+		if (p)
+		{
+			printf("'%s': '%s'", p->key, p->value);
+			f = 0;
+			for (y = x; y < ht->size; y++)
+			{
+				if (y == x)
+					continue;
+				if (ht->array[y])
+				{
+					f = 1;
+					break;
+				}
+			}
+			if (f)
 				printf(", ");
 		}
-		if (ht->array[x] && x < (ht->size - 1))
-			printf(", ");
 	}
 	printf("}");
 	printf("\n");
