0b99cb92e96ad7fe998e9f4f6015b3a799c7ca8a
snavy <1337snavy@gmail.com>
2022-11-25 06:21:46 +0000

    fixed files for ex07

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -53,7 +53,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			a->prev = r;
 			return (r);
 		}
-		x;
+		x++;
 	}
 	if (x == idx)
 		return (add_dnodeint_end(h, n));
