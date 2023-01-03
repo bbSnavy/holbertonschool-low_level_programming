de14094f2313638526cf3087dc23baf5cf294e28
snavy <1337snavy@gmail.com>
2023-01-03 17:12:00 +0100

    add: ex00

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include "search_algorithms.c"
+
+/**
+ * linear_search - function
+ * @array: []int
+ * @size: size_t
+ * @value: int
+ *
+ * Return: int
+ */
+int	linear_search(int *array, size_t size, int value)
+{
+	size_t	x;
+
+	if (array == 0)
+		return (-1);
+	for (x = 0; x < size; x++)
+		if (array[x] == value)
+			return (x);
+	return (-1);
+}
+
